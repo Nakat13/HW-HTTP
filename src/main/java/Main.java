90ab54f0ebf1dffecb1b84c04f48551f9388627e
nakat13 <denisovaekaterina13@ya.ru>
2022-05-26 10:06:33 +0300

    v.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,17 @@
-import org.apache.http.ParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.Locale.filter;
+import java.util.stream.Collectors;
 
 public class Main {
+    public static final ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) throws IOException {
         CloseableHttpClient httpClient = HttpClientBuilder.create()
@@ -25,23 +21,18 @@ public class Main {
                         .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
                         .build())
                 .build();
+
         HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
         CloseableHttpResponse response = httpClient.execute(request);
 
-        String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
-        System.out.println(body);
-       // String json = readString("");
-        JSONParser parser = new JSONParser();
-        try {
-            Object obj = parser.parse(new FileReader(body));
-            JSONObject jsonObject = (JSONObject) obj;
-            List<factInfo> factInfoList = new ArrayList<>();
-            factInfoList.add(new factInfo(jsonObject));
-            List<factInfo> filteredList = (List<factInfo>) factInfoList.stream()
-                    .filter(value -> (value.getUpvotes() != null) && (value.getUpvotes() > 0));
-            System.out.println(filteredList);
-        } catch (IOException | ParseException | org.json.simple.parser.ParseException e){
-            e.printStackTrace();
-        }
+        List<Post> posts = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<Post>>() {
+        });
+
+        List<Post> filteredPosts = (List<Post>) posts.stream()
+                .filter(value -> (value.getUpvotes() != null) && (value.getUpvotes() > 0)).collect(Collectors.toList());
+        filteredPosts.forEach(System.out::println);
+
+        response.close();
+        httpClient.close();
     }
 }
